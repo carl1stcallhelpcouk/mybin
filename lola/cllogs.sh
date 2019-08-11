d876c99276bdcb5b1a956e5201a3241e08cae3cd
Carl McAlwane <carl@1stcallhelp.co.uk>
2019-08-11 13:16:23 +0100

    Harmonised ${filelist}

--- lola/cllogs.sh
+++ lola/cllogs.sh
@@ -4,15 +4,16 @@ if [ $EUID != 0 ]; then
     sudo "$0" "$@"
     exit $?
 fi
+
 filelist=(/var/log/mail.log /var/log/mail.err /var/log/dovecot.log /var/log/mysql.err /var/log/syslog /var/log/Xorg.1.log /var/log/apache2/error.log /var/log/apache2/other_vhosts_access.log /var/log/mystatusd.log /var/log/mystatusd.err /var/log/mystatysd.last /var/log/apache2/access.log)
 
 for file in "${filelist[@]}"
 do
     if [ -r "${file}" ] ; then
     	printf "\nTruncating %s ...\n" "${file}"
-	sudo bash -c "echo "" > "${file}""
-   else
-	printf "\nSkipping %s ...\n" "${file}"
-   fi
+        sudo bash -c "echo "" > "${file}""
+    else
+        printf "\nSkipping %s ...\n" "${file}"
+    fi
 done
 if [[ "$1" == "--reboot" ]] ; then sudo reboot ; fi
--- lola/scanlog.sh
+++ lola/scanlog.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 #
-old_pwd=$(pwd)
-#cd /var/log
-
-filelist=(/var/log/mail.log /var/log/dovecot.log /var/log/mysql.err /var/log/syslog /var/log/Xorg.1.log)
+filelist=(/var/log/mail.log /var/log/mail.err /var/log/dovecot.log /var/log/mysql.err /var/log/syslog /var/log/Xorg.1.log /var/log/apache2/error.log /var/log/apache2/other_vhosts_access.log /var/log/mystatusd.log /var/log/mystatusd.err /var/log/mystatysd.last /var/log/apache2/access.log)
 
 for file in "${filelist[@]}" 
 do 
     if [ -r "${file}" ] ; then
-    	printf "\nScanning %s ...\n" "${file}"
-    	grep --color=always -i -B 5 "error\|warn\|fail\|missing\|grep (EE)" "${file}" | grep -v "fail2ban"
-   else
-	printf "\nSkipping %s ...\n" "${file}"
-   fi
+        printf "\nScanning %s ...\n" "${file}"
+        grep --color=always -i -B 5 "error\|warn\|fail\|missing\|grep (EE)" "${file}" | grep -v "fail2ban"
+    else
+        printf "\nSkipping %s ...\n" "${file}"
+    fi
 done
-
-#cd "${old_pwd}"
